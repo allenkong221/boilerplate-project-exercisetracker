25d0ee0c77ce6fecb29c6ccb9487d1f29de7a87d
allenkong221 <allen.kong@uconn.edu>
2022-08-31 18:12:25 -0400

    changed definitions to const

--- schemas/exercise.js
+++ schemas/exercise.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-let exerciseSchema = new mongoose.Schema({
+const exerciseSchema = new mongoose.Schema({
   username: String,
   description: String,
   duration: Number,
